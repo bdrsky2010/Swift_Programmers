b052f3c95c93777918912667b0665cf104bae4a7
MJKim <qorhaqt@gmail.com>
2023-09-18 09:14:47 +0900

    Programmers

--- Programmers Lv1.playground/Pages/내적.xcplaygroundpage/Contents.swift
+++ Programmers Lv1.playground/Pages/내적.xcplaygroundpage/Contents.swift
@@ -3,7 +3,6 @@ import Foundation
 func solution(_ a:[Int], _ b:[Int]) -> Int {
 	var answer = 0
 	for index in 0..<a.count {
-		print(a[index] * b[index])
 		answer = answer + (a[index] * b[index])
 	}
 	return answer
