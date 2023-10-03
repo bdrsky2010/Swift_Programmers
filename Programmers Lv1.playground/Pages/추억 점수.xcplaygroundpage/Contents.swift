5751bd5ea400557181add6e16f84130a82a2a1a3
MJKim <qorhaqt@gmail.com>
2023-10-03 20:29:22 +0900

    Programmers

--- Programmers Lv1.playground/Pages/추억 점수.xcplaygroundpage/Contents.swift
+++ Programmers Lv1.playground/Pages/추억 점수.xcplaygroundpage/Contents.swift
@@ -16,4 +16,11 @@ func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
 		})
 	}
 }
-solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])
+
+// 다른 사람 풀이
+func solution2(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
+	let score: [String: Int] = Dictionary(uniqueKeysWithValues: zip(name, yearning))
+	return photo.map { $0.reduce(0) { $0 + (score[$1] ?? 0) } }
+}
+
+solution2(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])
