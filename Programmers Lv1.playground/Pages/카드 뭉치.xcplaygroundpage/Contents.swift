0713ade1c3b297c2e873a6262ee105e88d4bf1a7
MJKim <qorhaqt@gmail.com>
2023-10-05 22:34:56 +0900

    Programmers

--- Programmers Lv1.playground/Pages/카드 뭉치.xcplaygroundpage/Contents.swift
+++ Programmers Lv1.playground/Pages/카드 뭉치.xcplaygroundpage/Contents.swift
@@ -17,6 +17,12 @@ func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
 }
 
 // 리팩토링을 통한 다른 풀이
+// 처음에는 리턴해줄 문자열 타입의 배열에 조건이 성립됐을 때 값을 담아주고
+// 각각의 카드에서 해당 요소를 삭제해주고 for문이 다 돌고난 후 
+// goal과 sentence가 같다면 Yes
+// goal과 sentence가 같지않다면 No를 리턴했지만
+// 다시 생각해보니 요소를 담지 않고 for문을 돌며 goal의 요소와 각각의 card의 요소를 확인 후
+// card1과 card2 모두 goal의 요소가 포함되어 있지 않다면 No를 리턴해주는 방식으로 바꿨다.
 func solution2(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
 	var cards1:[String] = cards1
 	var cards2:[String] = cards2
