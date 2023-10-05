4a7a41f639c473643f4deb89d2ec1163d177cd95
MJKim <qorhaqt@gmail.com>
2023-10-05 22:36:14 +0900

    Programmers

--- Programmers Lv1.playground/Pages/카드 뭉치.xcplaygroundpage/Contents.swift
+++ Programmers Lv1.playground/Pages/카드 뭉치.xcplaygroundpage/Contents.swift
@@ -23,6 +23,7 @@ func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
 // goal과 sentence가 같지않다면 No를 리턴했지만
 // 다시 생각해보니 요소를 담지 않고 for문을 돌며 goal의 요소와 각각의 card의 요소를 확인 후
 // card1과 card2 모두 goal의 요소가 포함되어 있지 않다면 No를 리턴해주는 방식으로 바꿨다.
+// 이런식으로 코드를 바꿔준다면 처음 풀이보다 시간도 메모리도 절약할 수 있다
 func solution2(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
 	var cards1:[String] = cards1
 	var cards2:[String] = cards2
